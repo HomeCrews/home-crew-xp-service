26fd1a4be4949830f418b4ab3b6be93a1beea321
mThanuj <mullagurithanuj0@gmail.com>
2026-09-11 21:06:10 +0530

    init

---
+++ src/main/java/com/homecrew/xpservice/XpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.homecrew.xpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homecrew/xpservice/XpserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecrew.xpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
