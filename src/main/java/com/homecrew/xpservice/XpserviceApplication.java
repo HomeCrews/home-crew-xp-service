87cf298b1c17a58c65d9e4840d658da693ec12de
mThanuj <mullagurithanuj0@gmail.com>
2026-09-16 15:03:20 +0530

    style: apply spotless formatting
    google-java-format AOSP: tabs to 4 spaces, collapsed empty bodies. No
    behaviour change. Equivalent to the reformat already applied to
    home-crew-admin-service.

--- src/main/java/com/homecrew/xpservice/XpserviceApplication.java
+++ src/main/java/com/homecrew/xpservice/XpserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class XpserviceApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(XpserviceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(XpserviceApplication.class, args);
+    }
 }
